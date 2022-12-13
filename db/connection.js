597ee0852f25b12885f4be55d3deac846b7eba2d
Reggie P <reginaldpitts1@gmail.com>
2022-12-13 12:04:41 -0600

    sql files

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,12 @@
 // require the package
 const mysql = require('mysql');
 
+const appConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'corporation',
+    password: '82Reggie82!',
+});
+
+// export the function
+module.exports = appConnection;
