9ce66d2b7335d5cb7116c984dc216a11150af29f
Reggie P <reginaldpitts1@gmail.com>
2022-12-12 18:03:42 -0600

    edit server code

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-// declareconst as variable for requirer
+// dependencies imported into application
 const inquirer = require('inquirer');
 
-// link to the connection.js file in the db folder
+// controllers
 const db = require('./db/connection');
+
+// Functions
+const mainMenu = async () => {
+    console.log('Welcome to the Employee Teacker Application ');
+
+    // Use inquirer to prompt users with different options/choices
+    await inquirer
+      .prompt({
+        name: "",
+        type: "list",
+        message: "Main Menu",
+        choices: [
+            "View All Employees",
+            "View All Employees by ROLE",
+            "View All Employees by DEPARTMENT",
+            "View All Employees by MANAGER",
+            "Add Employee",
+            "Add Department",
+            "Update Employee ROLE",
+            "Update Employee MANAGER",
+            "Delete Employee",
+            "Delete ROLE",
+            "Delete DEPARTMENT",
+            "View Department Budgets",
+        ],
+      })
+      .then((answer) => {
+        // user answer into the switch case
+      })
+}
