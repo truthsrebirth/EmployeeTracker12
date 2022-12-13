faccca4a343329e9cc7d67c6e638852e260d9e12
Reggie P <reginaldpitts1@gmail.com>
2022-12-13 13:16:57 -0600

    update connection js

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
-// require the package
-const mysql = require('mysql');
+// require the mysql package from package json file
+const mysql = require('mysql2');
 
 const appConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'corporation',
-    password: '82Reggie82!',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PW,
 });
 
 // export the function
